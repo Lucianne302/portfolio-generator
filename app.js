e020aba107e0b61ccade419cb9c64b4fa0657990
lucianne302 <lucianne21@comcast.net>
2020-10-02 16:52:25 -0400

    created answer validation

--- app.js
+++ app.js
@@ -9,12 +9,28 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: projectInput => {
+              if (projectInput){
+                return true; 
+              } else {
+                console.log('Please enter your project name!');
+                return false;
+              }
+            }
           },
           {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescriptionInput => {
+              if (projectDescriptionInput){
+                return true; 
+              } else {
+                console.log('Please enter your project description!');
+                return false;
+              }
+            }
           },
           {
             type: 'checkbox',
@@ -25,7 +41,15 @@ const promptProject = portfolioData => {
           {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+              if (linkInput){
+                return true; 
+              } else {
+                console.log('Please enter your GitHub link!');
+                return false;
+              }
+            }
           },
           {
             type: 'confirm',
@@ -56,12 +80,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput){
+                return true; 
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
         }, 
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your Github Username'
+            message: 'Enter your Github Username',
+            validate: githubInput => {
+              if (githubInput){
+                return true; 
+              } else {
+                console.log('Please enter your Username!');
+                return false;
+              }
+            }
         },
         {
             type: 'input',
