a18e7e848ac11a6f254effac8d13e97cc3d24c4c
lucianne302 <lucianne21@comcast.net>
2020-10-03 11:41:04 -0400

    added generate-site

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const fs = require ('fs'); 
+//const fs = require ('fs'); 
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 const inquirer = require ('inquirer'); 
@@ -78,12 +79,6 @@ const promptProject = portfolioData => {
     })
 };
 
-// const mockData = {
-//   name: 'Lucianne',
-//   github: 'Lucianne302',
-//   projects: []
-// }
-
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -133,17 +128,49 @@ const promptUser = () => {
     ]);
 };
 
-//const pageHTML = generatePage(mockData);
+ const mockData = {
+   name: 'Lucianne',
+   github: 'Lucianne302',
+   projects: []
+ }
 
+const pageHTML = generatePage(mockData);
+//const pageHTML = generatePage(portfolioData); 
+ 
  promptUser()
    .then(promptProject)
    .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData); 
 
+    // fs.writeFile('./dist/index.html', pageHTML, err => {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    
+    // fs.copyFile('./src/style.css', './dist/style.css', err => {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
+    //   console.log('Style sheet copied successfully!');
+    //   });
+      
+    // });
 
-fs.writeFile('./index.html', pageHTML, err => {
-     if (err) throw new Error(err);
-
-     console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
